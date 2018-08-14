9f85a76c1ff18d739fd3ab512825faab3f1a43f4
zaxirok <vvilmervd@gmail.com>
2018-08-14 01:38:46 -0600

    routes

--- clientTubeKids/src/app/app.module.ts
+++ clientTubeKids/src/app/app.module.ts
@@ -1,18 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import {FormsModule} from "@angular/forms";
+import {HttpModule} from "@angular/http";
+
+//routes
+import { app_routing }from "./app.routes";
 
 import { AppComponent } from './app.component';
 import { UsersService } from './services/users.service';
 import { RegisterComponent } from './component/register/register.component';
 import { LoginComponent } from './component/login/login.component';
+import { HomeComponent } from './component/home/home.component';
+import { InicioComponent } from './component/inicio/inicio.component';
+
+//import { NavbarInicioComponent } from './component/navbar-inicio/navbar-inicio.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     RegisterComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent,
+    InicioComponent,
+    //NavbarInicioComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    app_routing,
+    HttpClientModule, 
+    FormsModule,
+    ReactiveFormsModule, 
+    HttpModule
+    
   ],
   providers: [UsersService],
   bootstrap: [AppComponent]
