dd93437adee329e5c1b479f51d5ff740a5f2477a
zaxirok <vvilmervd@gmail.com>
2018-08-20 02:28:07 -0600

    aunthentification and Permision of urls

--- clientTubeKids/src/app/app.component.ts
+++ clientTubeKids/src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { UsersService} from './services/users.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [UsersService]
 })
 export class AppComponent {
   title = 'clientTubeKids';
+  constructor(private userServices: UsersService, private router : Router) {
+    
+  
 }
+Logout(){
+  localStorage.removeItem('userToken');
+this.router.navigate(['/login']);
+}
+}
+
+  
+
+ 
